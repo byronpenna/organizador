c236ff3345f18a9a3e554caf9b61e96d091c2fab
Byron Peña <byronpenna@gmail.com>
2014-04-10 15:25:30 -0600

    view almost finish

--- app/View.php
+++ app/View.php
@@ -7,8 +7,22 @@ class View
 
 	}
 	function render($view){
-		$sourceView = ROOT."site".DS.$view.".html";
-		$view = file_get_contents($sourceView);
+		$sourceView = SITE.$view.".html";
+		$view 		= file_get_contents($sourceView);
+		$keys 		= $this->getJson("map1.json");
+		$view 		= $this->setKeys($keys,$view);
 		echo $view;
 	}
+	// funciones de plantilla
+		function getJson($name){
+			$json = file_get_contents(MAP.$name);
+			$json = json_decode($json);
+			return $json;
+		}
+		function setKeys($keys,$view){
+			foreach ($keys as $key => $value) {
+				$view = str_replace("{@".$key."}", $value, $view);
+			}
+			return $view;
+		}
 }
--- app/paths.php
+++ app/paths.php
@@ -3,3 +3,5 @@
 	define('MODELS', ROOT . 'models' . DS);
 	define('VIEWS', ROOT . 'views' . DS);
 	define('PLUGINS', ROOT . 'plugins' . DS);
+	define('SITE',ROOT.'site'.DS);
+	define('MAP',SITE.'map'.DS);
